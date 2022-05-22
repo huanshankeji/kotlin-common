f27af0c63f4f5846092ce70ce0033e3dca755d84
Shreck Ye <ShreckYe@gmail.com>
2022-05-22 19:09:12 +0800

    Use Kotlin 1.6.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.6.21"))
-    implementation("com.huanshankeji:common-gradle-dependencies:0.1.0-20220522")
-    implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.1")
+    implementation(kotlin("gradle-plugin", "1.6.10"))
+    implementation("com.huanshankeji:common-gradle-dependencies:0.1.0-20220522-kotlin-1.6.10")
+    implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.1-kotlin-1.6.10")
 }
