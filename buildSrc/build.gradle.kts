3a421e6291053a4432dc8e88ed9726d4dcad3d7f
Shreck Ye <ShreckYe@gmail.com>
2022-05-22 14:16:23 +0800

    Use "common-gradle-dependencies:0.1.0-20220522"

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.6.21"))
-    implementation("com.huanshankeji:common-gradle-dependencies:0.1.0-20220520")
+    implementation("com.huanshankeji:common-gradle-dependencies:0.1.0-20220522")
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.1")
 }
