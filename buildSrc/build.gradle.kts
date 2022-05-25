6660b661dc3b0d64b78b0e413acb98d826ad896d
Shreck Ye <ShreckYe@gmail.com>
2022-05-25 21:54:00 +0800

    Enable publishing to Maven Central via Sonatype OSSRH

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,6 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin", "1.6.10"))
     implementation("com.huanshankeji:common-gradle-dependencies:0.1.0-20220522-kotlin-1.6.10")
-    implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.1-kotlin-1.6.10")
+    implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.2-kotlin-1.6.10")
+    //implementation("io.codearte.gradle.nexus:gradle-nexus-staging-plugin:0.30.0")
 }
--- buildSrc/src/main/kotlin/common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/common-conventions.gradle.kts
@@ -1,2 +1,56 @@
+plugins {
+    `maven-publish`
+    signing
+}
+
 //group = "com.huanshankeji"
 version = projectVersion
+
+signing {
+    sign(publishing.publications)
+}
+
+publishing {
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            credentials {
+                username = project.property("ossrhUsername") as String
+                password = project.property("ossrhPassword") as String
+            }
+        }
+    }
+
+    publications {
+        withType<MavenPublication> {
+            pom {
+                name.set("Huanshankeji Kotlin Common")
+                description.set("Huanshankeji's common code libraries in Kotlin")
+                val githubUrl = "https://github.com/huanshankeji/kotlin-common"
+                url.set(githubUrl)
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ShreckYe")
+                        name.set("Shreck Ye")
+                        email.set("ShreckYe@gmail.com")
+                    }
+                }
+                scm {
+                    val scmString = "scm:git:$githubUrl.git"
+                    connection.set(scmString)
+                    developerConnection.set(scmString)
+                    url.set(githubUrl)
+                }
+            }
+        }
+    }
+}
