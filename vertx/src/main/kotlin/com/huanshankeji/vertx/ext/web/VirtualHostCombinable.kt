fab8eca15e1297533cd2034bafd1eff8ef35c87b
Shreck Ye <ShreckYe@gmail.com>
2022-07-03 15:52:35 +0800

    Add `SingleVirtualHostCombinable` and update the project version

--- vertx/src/main/kotlin/com/huanshankeji/vertx/ext/web/VirtualHostCombinable.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/ext/web/VirtualHostCombinable.kt
@@ -1,7 +1,22 @@
 package com.huanshankeji.vertx.ext.web
 
+import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
 
 interface VirtualHostCombinable {
     fun Router.routesOnVirtualHost()
+}
+
+interface SingleVirtualHostCombinable : VirtualHostCombinable {
+    override fun Router.routesOnVirtualHost() {
+        mountSubRouter("/", subRouter()).virtualHost(virtualHost)
+    }
+
+    val virtualHost: String
+
+    val vertx: Vertx
+    fun subRouter(): Router =
+        Router.router(vertx).apply { routes() }
+
+    fun Router.routes()
 }
