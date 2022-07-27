b0b3fc3f71bc958ec191b2e57d6a1a8559ea841f
Shreck Ye <ShreckYe@gmail.com>
2022-07-27 20:05:59 +0800

    Update the Kotlin version to 1.7.10

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.6.10"))
+    implementation(kotlin("gradle-plugin", "1.7.10"))
     implementation("com.huanshankeji:common-gradle-dependencies:0.1.1-20220527-kotlin-1.6.10")
     implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.5-kotlin-1.6.10")
 }
