38b5f47184f0cf088a2a9bf295f68a2e4c253886
Shreck Ye <ShreckYe@gmail.com>
2022-07-28 20:33:47 +0800

    Comment out the Ktor client dependency in ":ktor" as it's not currently used and remove the TODOs added in the previous commit

--- ktor/client/build.gradle.kts
+++ ktor/client/build.gradle.kts
@@ -6,7 +6,6 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                // TODO: this is not needed here
                 //implementation(commonDependencies.ktor.client.core())
             }
         }
