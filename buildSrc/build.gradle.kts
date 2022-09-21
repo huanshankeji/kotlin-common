64fd3c38449aeb8c0ccd18965956e323e6d1d0dd
Shreck Ye <ShreckYe@gmail.com>
2022-09-21 18:52:50 +0800

    Use "com.huanshankeji.team:gradle-plugins:0.2.0-SNAPSHOT" to publish to GitHub Packages
    The corresponding commit: https://github.com/huanshankeji/gradle-common/commit/14669c8fd63b4f605fb38e7f59148143980ba9bd

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     implementation(kotlin("gradle-plugin", "1.7.10"))
     implementation("com.huanshankeji:common-gradle-dependencies:0.3.1-20220728")
-    implementation("com.huanshankeji:kotlin-common-gradle-plugins:0.1.9")
+    implementation("com.huanshankeji.team:gradle-plugins:0.2.0-SNAPSHOT") // TODO: use a stable version
 }
