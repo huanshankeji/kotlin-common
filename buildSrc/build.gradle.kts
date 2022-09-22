1b790eaa31af7d36e77da11ad89047fd0ec9bc91
Shreck Ye <ShreckYe@gmail.com>
2022-09-22 14:16:51 +0800

    Add the GitHub Packages Maven repository for "gradle-common"  and test it
    The corresponding commit: https://github.com/huanshankeji/gradle-common/commit/471ff164bc8ae512f60b7c009f3bb85b955d689c

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,10 +5,17 @@ plugins {
 repositories {
     mavenLocal()
     gradlePluginPortal()
+    maven {
+        url = uri("https://maven.pkg.github.com/huanshankeji/gradle-common")
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+        }
+    }
 }
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.7.10"))
     implementation("com.huanshankeji:common-gradle-dependencies:0.3.1-20220728")
-    implementation("com.huanshankeji.team:gradle-plugins:0.2.0-SNAPSHOT") // TODO: use a stable version
+    implementation("com.huanshankeji.team:gradle-plugins:0.2.0-SNAPSHOT") // TODO: don't use a SNAPSHOT version
 }
