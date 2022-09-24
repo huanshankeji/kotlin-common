4cd0a39839ff7a4eae7305f2ef716db46fbc92b4
Shreck Ye <ShreckYe@gmail.com>
2022-09-24 20:22:06 +0800

    Bump build dependency versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,6 +16,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.7.10"))
-    implementation("com.huanshankeji:common-gradle-dependencies:0.3.1-20220728")
-    implementation("com.huanshankeji.team:gradle-plugins:0.3.0-SNAPSHOT") // TODO: don't use a SNAPSHOT version
+    implementation("com.huanshankeji:common-gradle-dependencies:0.3.2-20220728")
+    implementation("com.huanshankeji.team:gradle-plugins:0.3.1")
 }
