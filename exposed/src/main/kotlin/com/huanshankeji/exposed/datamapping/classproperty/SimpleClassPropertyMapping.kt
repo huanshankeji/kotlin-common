huanshankeji/kotlin-common

commit 766018989e32f9e053f3fd3e247b98502c223825

Reorganize and rename the packages and interfaces, improve `columnsForAllSubclasses`, and remove the `ColumnSetT` type parameter in `SimpleDataUpdateMapper`

exposed/src/main/kotlin/com/huanshankeji/exposed/datamapping/classproperty/SimpleClassPropertyMapping.kt
@@ -1,5 +1,6 @@
-package com.huanshankeji.exposed.classpropertymapping
+package com.huanshankeji.exposed.datamapping.classproperty
 
+import com.huanshankeji.exposed.datamapping.SimpleDataMapper
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
@@ -10,29 +11,13 @@ import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 import kotlin.sequences.Sequence
 
-fun interface SimpleClassPropertyQueryMapper<Data : Any> {
-    fun resultRowToData(resultRow: ResultRow): Data
-}
-
-fun interface SimpleClassPropertyUpdateMapper<Data : Any, ColumnSetT : ColumnSet> {
-    fun updateBuilderSetter(data: Data): ColumnSetT.(UpdateBuilder<*>) -> Unit = {
-        setUpdateBuilder(data, it)
-    }
-
-    fun setUpdateBuilder(data: Data, updateBuilder: UpdateBuilder<*>)
-}
-
-interface SimpleClassPropertyMapper<Data : Any, ColumnSetT : ColumnSet> :
-    SimpleClassPropertyQueryMapper<Data>, SimpleClassPropertyUpdateMapper<Data, ColumnSetT>
-
 fun ResultRow.getValue(column: Column<*>): Any? =
     this[column].let {
         if (it is EntityID<*>) it.value else it
     }
 
 /** Nested classes are not supported. */
-interface ReflectionBasedSimpleClassPropertyMapper<Data : Any, TableT : Table> :
-    SimpleClassPropertyMapper<Data, TableT> {
+interface ReflectionBasedSimpleClassPropertyDataMapper<Data : Any> : SimpleDataMapper<Data> {
     val propertyAndColumnPairs: List<Pair<KProperty1<Data, *>, Column<*>>>
     val dataPrimaryConstructor: KFunction<Data>
 
@@ -48,8 +33,8 @@ interface ReflectionBasedSimpleClassPropertyMapper<Data : Any, TableT : Table> :
     }
 }
 
-inline fun <reified Data : Any, reified TableT : Table> reflectionBasedSimpleClassPropertyMapper(table: TableT): ReflectionBasedSimpleClassPropertyMapper<Data, TableT> =
-    object : ReflectionBasedSimpleClassPropertyMapper<Data, TableT> {
+inline fun <reified Data : Any> reflectionBasedSimpleClassPropertyDataMapper(table: Table): ReflectionBasedSimpleClassPropertyDataMapper<Data> =
+    object : ReflectionBasedSimpleClassPropertyDataMapper<Data> {
         private val clazz = Data::class
 
         // This property needs to initialize first.
@@ -78,13 +63,13 @@ inline fun <reified TableT : Table> getColumnByPropertyNameMapWithTypeParameter(
     getColumnPropertyByNameMap(TableT::class)
         .mapValues { it.value(table) }
 
-inline fun <reified D : Any, reified T : Table> reflectionBasedSimpleClassPropertyMapperForAlias(
-    tableClassPropertyMapper: ReflectionBasedSimpleClassPropertyMapper<D, T>, alias: Alias<T>
-): ReflectionBasedSimpleClassPropertyMapper<D, T> =
-    object : ReflectionBasedSimpleClassPropertyMapper<D, T> {
-        override val dataPrimaryConstructor = tableClassPropertyMapper.dataPrimaryConstructor
+inline fun <reified Data : Any, reified TableT : Table> reflectionBasedSimpleClassPropertyDataMapperForAlias(
+    tableDataMapper: ReflectionBasedSimpleClassPropertyDataMapper<Data>, alias: Alias<TableT>
+): ReflectionBasedSimpleClassPropertyDataMapper<Data> =
+    object : ReflectionBasedSimpleClassPropertyDataMapper<Data> {
+        override val dataPrimaryConstructor = tableDataMapper.dataPrimaryConstructor
         override val propertyAndColumnPairs =
-            tableClassPropertyMapper.propertyAndColumnPairs.map { it.first to alias[it.second] }
+            tableDataMapper.propertyAndColumnPairs.map { it.first to alias[it.second] }
     }
 
 

exposed/src/main/kotlin/com/huanshankeji/exposed/datamapping/DataMapperInterfaces.kt
@@ -1,0 +1,31 @@
+package com.huanshankeji.exposed.datamapping
+
+import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ColumnSet
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.statements.UpdateBuilder
+
+fun interface SimpleDataQueryMapper<Data : Any> {
+    fun resultRowToData(resultRow: ResultRow): Data
+}
+
+fun interface SimpleDataUpdateMapper<Data : Any> {
+    fun setUpdateBuilder(data: Data, updateBuilder: UpdateBuilder<*>)
+}
+
+fun <Data : Any, ColumnSetT : ColumnSet> SimpleDataUpdateMapper<Data>.updateBuilderSetter(data: Data):
+        ColumnSetT.(UpdateBuilder<*>) -> Unit = {
+    setUpdateBuilder(data, it)
+}
+
+interface SimpleDataMapper<Data : Any> :
+    SimpleDataQueryMapper<Data>, SimpleDataUpdateMapper<Data>
+
+
+interface DataQueryMapper<Data : Any> : SimpleDataQueryMapper<Data> {
+    val neededColumns: List<Column<*>>
+}
+
+typealias DataUpdateMapper<Data> = SimpleDataUpdateMapper<Data>
+
+interface DataMapper<Data : Any> : DataQueryMapper<Data>, SimpleDataMapper<Data>

exposed/src/main/kotlin/com/huanshankeji/exposed/datamapping/classproperty/ClassPropertyMapping.kt
@@ -1,7 +1,8 @@
-package com.huanshankeji.exposed.classpropertymapping
+package com.huanshankeji.exposed.datamapping.classproperty
 
-import com.huanshankeji.exposed.classpropertymapping.OnDuplicateColumnPropertyNames.*
-import com.huanshankeji.exposed.classpropertymapping.PropertyColumnMapping.*
+import com.huanshankeji.exposed.datamapping.DataMapper
+import com.huanshankeji.exposed.datamapping.classproperty.OnDuplicateColumnPropertyNames.*
+import com.huanshankeji.exposed.datamapping.classproperty.PropertyColumnMapping.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import org.slf4j.LoggerFactory
@@ -49,17 +50,17 @@ sealed class PropertyColumnMapping<Data : Any, PropertyData>(val property: KProp
                 val subclassMap: Map<KClass<out NotNullPropertyData>, Product<out NotNullPropertyData>>,
                 val sumTypeCaseConfig: SumTypeCaseConfig<NotNullPropertyData, CaseValue>
             ) : Adt<NotNullPropertyData>() {
-                val columnsForAllSubclasses = subclassMap.values.asSequence()
-                    .flatMap { it.nestedMappings.getNeededColumnSet() }
-                    .toSet() // Using `toSet` seems to be faster than `distinct`.
-                    .toList()
+                val columnsForAllSubclasses = buildSet {
+                    for (subclassProductMapping in subclassMap.values)
+                        addAll(subclassProductMapping.nestedMappings.getColumnSet())
+                }.toList()
             }
         }
     }
 
     // TODO: remove the temporary workaround `& Any` if and when the `Any` upper bound is removed.
     class Custom<Data : Any, PropertyData>(
-        property: KProperty1<Data, PropertyData>, val classPropertyMapper: ClassPropertyMapper<PropertyData & Any>
+        property: KProperty1<Data, PropertyData>, val dataMapper: DataMapper<PropertyData & Any>
     ) : PropertyColumnMapping<Data, PropertyData>(property)
 
     class Skip<Data : Any, PropertyData>(property: KProperty1<Data, PropertyData>) :
@@ -353,21 +354,13 @@ fun <Data : Any> getDefaultClassPropertyColumnMappings(
         customMappings
     )
 
-interface ClassPropertyQueryMapper<Data : Any> : SimpleClassPropertyQueryMapper<Data> {
-    val neededColumns: List<Column<*>>
-}
-
-typealias ClassPropertyUpdateMapper<Data> = ClassPropertyQueryMapper<Data>
-
-interface ClassPropertyMapper<Data : Any> : ClassPropertyQueryMapper<Data>, SimpleClassPropertyMapper<Data, ColumnSet>
-
 // TODO: decouple query mapper and update mapper.
 /** Supports classes with nested composite class properties and multiple tables */
-class ReflectionBasedClassPropertyMapper<Data : Any>(
+class ReflectionBasedClassPropertyDataMapper<Data : Any>(
     val clazz: KClass<Data>,
     val classPropertyColumnMappings: ClassPropertyColumnMappings<Data>,
-) : ClassPropertyMapper<Data> {
-    override val neededColumns = classPropertyColumnMappings.getNeededColumnSet().toList()
+) : DataMapper<Data> {
+    override val neededColumns = classPropertyColumnMappings.getColumnSet().toList()
     override fun resultRowToData(resultRow: ResultRow): Data =
         constructDataWithResultRow(clazz, classPropertyColumnMappings, resultRow)
 
@@ -414,7 +407,7 @@ fun <Data : Any> constructDataWithResultRow(
                     }
                 }
 
-                is Custom -> propertyColumnMapping.classPropertyMapper.resultRowToData(resultRow)
+                is Custom -> propertyColumnMapping.dataMapper.resultRowToData(resultRow)
                 is Skip -> null
             }
         @Suppress("UNCHECKED_CAST")
@@ -480,7 +473,7 @@ fun <Data : Any> setUpdateBuilder(
 
                 is Custom ->
                     // TODO: remove this cast if and when not only non-nullable data are supported.
-                    propertyColumnMapping.classPropertyMapper.setUpdateBuilder(
+                    propertyColumnMapping.dataMapper.setUpdateBuilder(
                         propertyData as (PropertyData & Any), updateBuilder
                     )
 
@@ -509,7 +502,7 @@ fun PropertyColumnMapping<*, *>.forEachColumn(block: (Column<*>) -> Unit) =
             }
         }
 
-        is Custom -> classPropertyMapper.neededColumns.forEach(block)
+        is Custom -> dataMapper.neededColumns.forEach(block)
         is Skip -> {}
     }
 
@@ -532,22 +525,22 @@ fun setUpdateBuilderColumnsToNulls(columns: List<Column<*>>, updateBuilder: Upda
         updateBuilder[column as Column<Any?>] = null
 }
 
-fun ClassPropertyColumnMappings<*>.getNeededColumnSet(): Set<Column<*>> =
+fun ClassPropertyColumnMappings<*>.getColumnSet(): Set<Column<*>> =
     buildSet { forEachColumn { add(it) } }
 
-inline fun <reified Data : Any> reflectionBasedClassPropertyMapper(
+inline fun <reified Data : Any> reflectionBasedClassPropertyDataMapper(
     tables: List<Table>,
     onDuplicateColumnPropertyNames: OnDuplicateColumnPropertyNames = CHOOSE_FIRST,
     customMappings: PropertyColumnMappings<Data> = emptyList()
-): ReflectionBasedClassPropertyMapper<Data> {
+): ReflectionBasedClassPropertyDataMapper<Data> {
     val clazz = Data::class
-    return ReflectionBasedClassPropertyMapper(
+    return ReflectionBasedClassPropertyDataMapper(
         clazz, getDefaultClassPropertyColumnMappings(clazz, tables, onDuplicateColumnPropertyNames, customMappings)
     )
 }
 
-inline fun <reified Data : Any/*, TableT : Table*/> reflectionBasedClassPropertyMapper(
+inline fun <reified Data : Any/*, TableT : Table*/> reflectionBasedClassPropertyDataMapper(
     table: Table,
     customMappings: PropertyColumnMappings<Data> = emptyList()
 ) =
-    reflectionBasedClassPropertyMapper(listOf(table), THROW, customMappings)
+    reflectionBasedClassPropertyDataMapper(listOf(table), THROW, customMappings)

commit 912cb4b7b3542bc73f1a423dd2b33fccf4a011d5

Fix a bug introduced in commit 766018989e32f9e053f3fd3e247b98502c223825 that the `TableT` reified type parameter is removed in `reflectionBasedSimpleClassPropertyDataMapper`

exposed/src/main/kotlin/com/huanshankeji/exposed/datamapping/classproperty/SimpleClassPropertyMapping.kt
@@ -33,7 +33,8 @@ interface ReflectionBasedSimpleClassPropertyDataMapper<Data : Any> : SimpleDataM
     }
 }
 
-inline fun <reified Data : Any> reflectionBasedSimpleClassPropertyDataMapper(table: Table): ReflectionBasedSimpleClassPropertyDataMapper<Data> =
+// The `TableT` reified type parameter is passed to the functions called and should not be removed.
+inline fun <reified Data : Any, reified TableT : Table> reflectionBasedSimpleClassPropertyDataMapper(table: TableT): ReflectionBasedSimpleClassPropertyDataMapper<Data> =
     object : ReflectionBasedSimpleClassPropertyDataMapper<Data> {
         private val clazz = Data::class
 