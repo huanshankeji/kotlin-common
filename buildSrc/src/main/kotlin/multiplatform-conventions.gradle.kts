86d040301965b71b391f3691f3b2bbf3815fafd0
Shreck Ye <ShreckYe@gmail.com>
2023-03-12 19:23:07 +0800

    Upgrade the Huanshankeji Gradle build dependencies to the latest release versions

--- buildSrc/src/main/kotlin/multiplatform-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/multiplatform-conventions.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("common-conventions-before")
-    id("com.huanshankeji.kotlin-multiplatform-sonatype-ossrh-publish-conventions")
     id("com.huanshankeji.kotlin-multiplatform-jvm-and-js-browser-conventions")
+    id("com.huanshankeji.kotlin-multiplatform-sonatype-ossrh-publish-conventions")
     id("common-conventions-after")
 }
 
