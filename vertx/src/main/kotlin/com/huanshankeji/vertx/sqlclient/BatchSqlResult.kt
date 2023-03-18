0a05c2b44c63f449d19f81ddf94785c4ce80a4bd
Shreck Ye <ShreckYe@gmail.com>
2023-03-18 11:46:47 +0800

    Simplify `batchSqlResultSequence` with `generateSequence`

--- vertx/src/main/kotlin/com/huanshankeji/vertx/sqlclient/BatchSqlResult.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/sqlclient/BatchSqlResult.kt
@@ -3,14 +3,11 @@ package com.huanshankeji.vertx.sqlclient
 import io.vertx.sqlclient.RowSet
 import io.vertx.sqlclient.SqlResult
 
-fun <SqlResultT : SqlResult<*>> SqlResultT.batchSqlResultSequence(): Sequence<SqlResultT> = sequence {
-    var currentSqlResult: SqlResultT? = this@batchSqlResultSequence
-
-    while (currentSqlResult !== null) {
-        yield(currentSqlResult)
-        currentSqlResult = currentSqlResult.next() as SqlResultT?
+fun <SqlResultT : SqlResult<*>> SqlResultT.batchSqlResultSequence(): Sequence<SqlResultT> =
+    generateSequence(this) {
+        @Suppress("UNCHECKED_CAST")
+        it.next() as SqlResultT?
     }
-}
 
 fun SqlResult<*>.batchSqlResultRowCountSequence() =
     batchSqlResultSequence().map { it.rowCount() }
