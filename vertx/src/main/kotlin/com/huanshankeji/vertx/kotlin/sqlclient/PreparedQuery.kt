b2c3878da8439d82db2c0153cd05a11b212eae9b
Shreck Ye <ShreckYe@gmail.com>
2023-03-19 01:56:18 +0800

    Refactor `executeBatchAwaitForRowSetSequence` into `executeBatchAwaitForSqlResultSequence`

--- vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/sqlclient/PreparedQuery.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/sqlclient/PreparedQuery.kt
@@ -2,8 +2,8 @@ package com.huanshankeji.vertx.kotlin.sqlclient
 
 import io.vertx.kotlin.coroutines.await
 import io.vertx.sqlclient.PreparedQuery
-import io.vertx.sqlclient.RowSet
+import io.vertx.sqlclient.SqlResult
 import io.vertx.sqlclient.Tuple
 
-suspend fun <R> PreparedQuery<RowSet<R>>.executeBatchAwaitForRowSetSequence(batch: List<Tuple>) =
+suspend fun <SqlResultT : SqlResult<*>> PreparedQuery<SqlResultT>.executeBatchAwaitForSqlResultSequence(batch: List<Tuple>) =
     executeBatch(batch).await().batchSqlResultSequence()
