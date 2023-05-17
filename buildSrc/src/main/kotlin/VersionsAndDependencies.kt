39adf620cada7a02b7b40ee1d67824aa7aa727b1
Shreck Ye <ShreckYe@gmail.com>
2023-05-18 02:59:01 +0800

    Bump Vert.x to 4.4.2 ahead of "common-gradle-dependencies"

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,7 +3,6 @@ import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.3.1-SNAPSHOT"
-
-val commonVersions = CommonVersions()
+val commonVersions = CommonVersions(vertx = "4.4.2") // TODO: use the default version when it's bumped
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
