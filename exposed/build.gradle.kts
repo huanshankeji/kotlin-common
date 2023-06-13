e52018d307daf766d1ad84dbd87f18e56270c664
Shreck Ye <ShreckYe@gmail.com>
2023-06-13 21:00:42 +0800

    Move code related to Exposed mapping to the [exposed-adt-mapping](https://github.com/huanshankeji/exposed-adt-mapping) project

--- exposed/build.gradle.kts
+++ exposed/build.gradle.kts
@@ -1,12 +1,7 @@
-import com.huanshankeji.cpnProject
-
 plugins {
     id("jvm-conventions")
 }
 
 dependencies {
     implementation(commonDependencies.exposed.core())
-
-    implementation(cpnProject(project, ":core"))
-    implementation(cpnProject(project, ":reflect"))
 }
