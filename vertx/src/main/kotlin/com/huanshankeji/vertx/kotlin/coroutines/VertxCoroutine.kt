4540d8faa941aecc0e1e256fc4a2050d3f2bd5a3
Shreck Ye <ShreckYe@gmail.com>
2023-11-13 00:51:39 +0800

    Resolve the errors introduced by changes in Vert.x (they should not be breaking changes in Java but breaking changes in Kotlin probably because of the Kotlin parser's limitation)

--- vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/coroutines/VertxCoroutine.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/coroutines/VertxCoroutine.kt
@@ -2,6 +2,7 @@ package com.huanshankeji.vertx.kotlin.coroutines
 
 import com.huanshankeji.kotlin.use
 import io.vertx.core.Future
+import io.vertx.core.Handler
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.await
@@ -9,6 +10,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import java.util.concurrent.Callable
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 
@@ -27,20 +29,21 @@ suspend inline fun <R> Vertx.use(block: (Vertx) -> R): R =
  * [blockingCode] returns the result when the operation completes instead of calling [Promise.complete].
  */
 suspend fun <T> Vertx.awaitExecuteBlocking(blockingCode: () -> T): T =
-    executeBlocking {
-        it.complete(blockingCode())
-    }.await()
+    executeBlocking(Callable {
+        blockingCode()
+    }).await()
 
+// TODO: this should probably be removed
 /**
  * Like [awaitExecuteBlocking] but [blockingCode] is a suspend function.
  */
 @Suppress("DeprecatedCallableAddReplaceWith")
-@Deprecated("This implementation is buggy. See https://github.com/vert-x3/vertx-lang-kotlin/pull/222/commits/fc3c5c5cc0c572eaddb3c2c37d07c696f75b4443#diff-162b76dc534138518a237d9a8ed527f1b3ecaca67385ea7d4357b6eff203f699R138-R217 for a fixed proposed version.")
+@Deprecated("This API is deprecated for removal. See https://github.com/vert-x3/wiki/wiki/4.4.5-Deprecations-and-breaking-changes#deprecation-of-execute-blocking-methods-with-a-handler-of-promise. Also, this implementation is buggy. See https://github.com/vert-x3/vertx-lang-kotlin/pull/222/commits/fc3c5c5cc0c572eaddb3c2c37d07c696f75b4443#diff-162b76dc534138518a237d9a8ed527f1b3ecaca67385ea7d4357b6eff203f699R138-R217 for a fixed proposed version.")
 suspend fun <T> Vertx.awaitSuspendExecuteBlocking(blockingCode: suspend () -> T): T =
     coroutineScope {
-        executeBlocking {
+        executeBlocking(Handler<Promise<T>> {
             launch { it.complete(blockingCode()) }
-        }.await()
+        }).await()
     }
 
 /**
