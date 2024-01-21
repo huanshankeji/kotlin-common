66ef40ba269c7d21994dc6ae6426b161967b4b92
Shreck Ye <ShreckYe@gmail.com>
2024-01-21 17:59:03 +0800

    Improve the readability of `toReadableStringByReflection` and add some more clarifying KDocs

--- reflect/src/jvmMain/kotlin/com/huanshankeji/kotlin/reflect/ReadableString.kt
+++ reflect/src/jvmMain/kotlin/com/huanshankeji/kotlin/reflect/ReadableString.kt
@@ -8,6 +8,8 @@ import kotlin.reflect.full.memberProperties
  * A useful utility function for debugging purposes.
  * It produces the string produced by [toString] if the class were a data class with all public properties recursively.
  * Note the data should not have cyclic references, otherwise the function call will not terminate.
+ * Note that if a class has an overriden [toString] method (for example a data class), it will be called directly,
+ * so if it contains a public property whose class doesn't have an overriden [toString] method the default [Any.toString] will be called.
  */
 fun Any?.toReadableStringByReflection(): String =
     if (this === null)
@@ -17,10 +19,10 @@ fun Any?.toReadableStringByReflection(): String =
         val clazz = this::class as KClass<Any>
         if (clazz.java.methods.find { it.name == "toString" && it.parameterCount == 0 }!!.declaringClass == Any::class.java)
             "${clazz.simpleName}(${
-                clazz.memberProperties.asSequence().filter { it.visibility == KVisibility.PUBLIC }.joinToString(", ") {
-                    "${it.name}=${
+                clazz.memberProperties.asSequence().filter { it.visibility == KVisibility.PUBLIC }.joinToString(", ") { kProperty1 ->
+                    "${kProperty1.name}=${
                         try {
-                            it.invoke(this).toReadableStringByReflection()
+                            kProperty1(this).toReadableStringByReflection()
                         } catch (t: Throwable) {
                             t.printStackTrace()
                             t
