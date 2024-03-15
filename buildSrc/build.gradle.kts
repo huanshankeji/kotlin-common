7cef3c6a89d39fc86821c6e011348d895d59cc5f
Shreck Ye <ShreckYe@gmail.com>
2024-03-15 09:37:05 +0800

    Bump "common-gradle-dependencies" to v0.7.1-20240314

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,6 +16,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "1.9.23"))
-    implementation("com.huanshankeji:common-gradle-dependencies:0.7.1-20240314-boostrap")
+    implementation("com.huanshankeji:common-gradle-dependencies:0.7.1-20240314")
     implementation("com.huanshankeji.team:gradle-plugins:0.5.1")
 }
