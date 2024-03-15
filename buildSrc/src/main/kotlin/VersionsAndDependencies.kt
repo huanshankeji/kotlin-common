e99fb4223df5db89b78a9d3aeb498e897de0b4d1
Shreck Ye <ShreckYe@gmail.com>
2024-03-15 08:41:42 +0800

    Add back a missing newline

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -3,6 +3,7 @@ import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.4.0-SNAPSHOT"
+
 val commonVersions = CommonVersions()
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
