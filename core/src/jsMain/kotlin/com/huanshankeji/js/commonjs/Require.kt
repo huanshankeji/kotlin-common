ae528e63a3d8fbbe421a4bac23a55f1f28140905
Shreck Ye <ShreckYe@gmail.com>
2024-04-25 20:47:08 +0800

    Add the CommonJS `require`

---
+++ core/src/jsMain/kotlin/com/huanshankeji/js/commonjs/Require.kt
@@ -0,0 +1,4 @@
+package com.huanshankeji.js.commonjs
+
+// see: https://stackoverflow.com/questions/40270731/what-does-require-actually-return-the-file-or-the-function#:~:text=The%20require(...),case%20its%20the%20Profile%20function.
+external fun require(module: String): dynamic
