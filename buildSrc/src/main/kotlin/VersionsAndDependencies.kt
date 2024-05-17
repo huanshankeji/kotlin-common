8f7260aff932048835cbcabb3e54bd4d61d7f661
Shreck Ye <ShreckYe@gmail.com>
2024-05-17 13:25:31 +0800

    Update the project version

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -2,7 +2,7 @@ import com.huanshankeji.CommonDependencies
 import com.huanshankeji.CommonGradleClasspathDependencies
 import com.huanshankeji.CommonVersions
 
-val projectVersion = "0.4.0-SNAPSHOT"
+val projectVersion = "0.5.0-SNAPSHOT"
 
 val commonVersions = CommonVersions(arrow = "2.0.0-alpha.1") // for Wasm JS
 val commonDependencies = CommonDependencies(commonVersions)
