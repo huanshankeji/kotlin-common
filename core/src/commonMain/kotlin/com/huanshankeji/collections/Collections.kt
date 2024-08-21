c54027aca5bd71d052bb6d2177d87abb0ed4ee8c
Shreck Ye <ShreckYe@gmail.com>
2024-08-22 07:31:51 +0800

    Add `Iterable<T>.singleOrNullIfEmpty`

--- core/src/commonMain/kotlin/com/huanshankeji/collections/Collections.kt
+++ core/src/commonMain/kotlin/com/huanshankeji/collections/Collections.kt
@@ -8,3 +8,7 @@ fun <T> List<T>.eachCount() =
     groupBy { it }.mapValues { it.value.size }
 // This is tested to be faster than the implementation below in some scenarios.
 //groupingBy { it }.eachCount()
+
+// see https://discuss.kotlinlang.org/t/singleornull-whats-your-expectation/12085/14
+fun <T> Iterable<T>.singleOrNullIfEmpty() =
+    if (none()) null else single()
