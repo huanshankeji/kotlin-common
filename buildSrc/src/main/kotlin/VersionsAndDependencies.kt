5049c67a4c08f6a51fd4f52895042d7f28ef500e
Shreck Ye <ShreckYe@gmail.com>
2024-10-17 18:37:05 +0800

    Bump dependency versions in "VersionsAndDependencies.kt"

--- buildSrc/src/main/kotlin/VersionsAndDependencies.kt
+++ buildSrc/src/main/kotlin/VersionsAndDependencies.kt
@@ -4,11 +4,11 @@ import com.huanshankeji.CommonVersions
 
 val projectVersion = "0.5.0-SNAPSHOT"
 
-val commonVersions = CommonVersions(arrow = "2.0.0-alpha.3") // for Wasm JS
+val commonVersions = CommonVersions(arrow = "2.0.0-alpha.4") // for Wasm JS
 val commonDependencies = CommonDependencies(commonVersions)
 val commonGradleClasspathDependencies = CommonGradleClasspathDependencies(commonVersions)
 
 object DependencyVersions {
     val protobufPlugin = "0.9.4"
-    val protobuf = "3.25.2"
+    val protobuf = "3.25.2" // Not bumped. The version is a bit chaotic.
 }
