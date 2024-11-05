39fb14c48df950e10371b6daaf75bcba2d72b1ba
Shreck Ye <ShreckYe@gmail.com>
2024-11-05 12:19:33 +0800

    Initially add code to generate KDoc HTML with Dokka

---
+++ buildSrc/src/main/kotlin/dokka-convention.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("org.jetbrains.dokka")
+}
+
+dokka {
+    // TODO
+    /*
+    moduleName.set("Huanshankeji Kotlin Common")
+    dokkaSourceSets.named("main") {
+        includes.from("README.md")
+        sourceLink {
+            localDirectory.set(file("src/main/kotlin"))
+            remoteUrl("https://github.com/huanshankeji/kotlin-common/tree/v${version}")
+            remoteLineSuffix.set("#L")
+        }
+        pluginsConfiguration.html {
+            footerMessage.set("(c) Yongshun Ye")
+        }
+    }
+    */
+}
