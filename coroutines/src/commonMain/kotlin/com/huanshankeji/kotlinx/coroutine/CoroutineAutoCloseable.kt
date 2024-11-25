68d100ff8a42769b93912ae3aba865fbc8a984eb
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-25 16:31:12 +0800

    Add a `CoroutineAutoCloseable` interface, update `com.huanshankeji.kotlin.use`, and add `com.huanshankeji.kotlinx.coroutine.use`

---
+++ coroutines/src/commonMain/kotlin/com/huanshankeji/kotlinx/coroutine/CoroutineAutoCloseable.kt
@@ -0,0 +1,17 @@
+package com.huanshankeji.kotlinx.coroutine
+
+import com.huanshankeji.ExperimentalApi
+import com.huanshankeji.kotlin.use
+
+/**
+ * @see AutoCloseable
+ * Also see https://github.com/Kotlin/kotlinx.coroutines/issues/1191.
+ * Made a functional interface so that there is no need to add a function like the [AutoCloseable] one.
+ */
+@ExperimentalApi
+fun interface CoroutineAutoCloseable {
+    suspend fun close()
+}
+
+suspend inline fun <T : CoroutineAutoCloseable?, R> T.use(block: (T) -> R): R =
+    use(block, { this?.close() })
