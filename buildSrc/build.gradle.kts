038b7c85f5ee7961f196fe7a6ae9b6a556a5ea20
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-12-04 20:56:59 +0800

    Bump common-gradle-dependencies to v0.9.0-20241203-SNAPSHOT

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", "2.1.0"))
-    implementation("com.huanshankeji:common-gradle-dependencies:0.8.0-20241016") // don't use a snapshot version in a main branch
-    implementation("com.huanshankeji.team:gradle-plugins:0.8.0") // don't use a snapshot version in a main branch
+    implementation("com.huanshankeji:common-gradle-dependencies:0.9.0-20241203-SNAPSHOT") // TODO don't use a snapshot version in a main branch
+    implementation("com.huanshankeji.team:gradle-plugins:0.9.0-SNAPSHOT") // TODO don't use a snapshot version in a main branch
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.0.0-Beta")
 }
