17e1ac96d6e5c7b38828e7ed8cd2059e7a54dd07
Yongshun Ye <ShreckYe@gmail.com>
2025-10-17 09:15:15 +0800

    Initially migrate to Vert.x 5, fixing compile errors and tests

--- vertx/src/main/kotlin/com/huanshankeji/vertx/core/CombinedVerticle.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/core/CombinedVerticle.kt
@@ -24,8 +24,9 @@ interface CombinedVerticleFunctions : Verticle {
         @Suppress("UNCHECKED_CAST")
         subVerticlesStop(stopPromise as Promise<Void?>)
 
+    @Deprecated("APIs using `Promise` are deprecated. See https://vertx.io/docs/guides/vertx-5-migration-guide/#_embracing_the_future_model .")
     fun <E> List<E>.runAllWithPromise(block: E.(promise: Promise<Void?>) -> Unit, promise: Promise<Void?>) {
-        Future.all(map {
+        Future.all<E>(map {
             @Suppress("NAME_SHADOWING") val promise = Promise.promise<Void?>()
             vertx.runOnContext { _ -> it.block(promise) }
             //it.block(promise)
--- vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/coroutines/VertxCoroutine.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/coroutines/VertxCoroutine.kt
@@ -2,7 +2,6 @@ package com.huanshankeji.vertx.kotlin.coroutines
 
 import com.huanshankeji.kotlin.use
 import io.vertx.core.Future
-import io.vertx.core.Handler
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.kotlin.coroutines.coAwait
@@ -28,20 +27,22 @@ suspend inline fun <R> Vertx.use(block: (Vertx) -> R): R =
  * [blockingCode] returns the result when the operation completes instead of calling [Promise.complete].
  */
 suspend fun <T> Vertx.awaitExecuteBlocking(blockingCode: () -> T): T =
-    executeBlocking(Callable {
-        blockingCode()
-    }).coAwait()
+    executeBlocking(blockingCode).coAwait()
 
-// TODO: this should probably be removed
+// This should be removed.
 /**
  * Like [awaitExecuteBlocking] but [blockingCode] is a suspend function.
  */
-@Suppress("DeprecatedCallableAddReplaceWith")
-@Deprecated("This API is deprecated for removal. See https://github.com/vert-x3/wiki/wiki/4.4.5-Deprecations-and-breaking-changes#deprecation-of-execute-blocking-methods-with-a-handler-of-promise. Also, this implementation is buggy. See https://github.com/vert-x3/vertx-lang-kotlin/pull/222/commits/fc3c5c5cc0c572eaddb3c2c37d07c696f75b4443#diff-162b76dc534138518a237d9a8ed527f1b3ecaca67385ea7d4357b6eff203f699R138-R217 for a fixed proposed version.")
+@Deprecated("This API is deprecated for removal. " +
+        "See https://github.com/vert-x3/wiki/wiki/4.4.5-Deprecations-and-breaking-changes#deprecation-of-execute-blocking-methods-with-a-handler-of-promise " +
+        "and https://vertx.io/docs/guides/vertx-5-migration-guide/#_removal_of_execute_blocking_methods_with_a_handler_of_promise . " +
+        "Please rewrite your code to pass only the blocking parts to `awaitExecuteBlocking`. " +
+        "Also, this implementation is buggy. See https://github.com/vert-x3/vertx-lang-kotlin/pull/222/commits/fc3c5c5cc0c572eaddb3c2c37d07c696f75b4443#diff-162b76dc534138518a237d9a8ed527f1b3ecaca67385ea7d4357b6eff203f699R138-R217 for a fixed proposed version.")
 suspend fun <T> Vertx.awaitSuspendExecuteBlocking(blockingCode: suspend () -> T): T =
     coroutineScope {
-        executeBlocking(Handler<Promise<T>> {
-            launch { it.complete(blockingCode()) }
+        executeBlocking({
+            throw NotImplementedError("Please rewrite your code to pass only the blocking parts to `awaitExecuteBlocking`.")
+            //runBlocking { blockingCode() } // This implementation is meaningless.
         }).coAwait()
     }
 
@@ -72,4 +73,4 @@ fun <T> CoroutineScope.coroutineToFuture(
  * @see kotlinx.coroutines.awaitAll
  */
 suspend fun <T> List<Future<T>>.awaitAll(): List<T> =
-    Future.all(this).coAwait().list()
+    Future.all<T>(this).coAwait().list()
