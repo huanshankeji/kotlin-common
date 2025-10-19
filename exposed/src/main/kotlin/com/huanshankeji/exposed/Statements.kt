7323818d283489a09f2d88aeed9bfc640216e813
Yongshun Ye <ShreckYe@gmail.com>
2025-10-19 18:09:22 +0800

    Initially deprecate the statement creation APIs to ease migration for consumers
    Quite some `@Deprecated` code is completed by Copilot.

--- exposed/src/main/kotlin/com/huanshankeji/exposed/Statements.kt
+++ exposed/src/main/kotlin/com/huanshankeji/exposed/Statements.kt
@@ -16,6 +16,9 @@ import org.jetbrains.exposed.v1.jdbc.selectAll
 @InternalApi
 const val SELECT_DSL_DEPRECATION_MESSAGE =
     "As part of Exposed SELECT DSL design changes, this will be removed in future releases."
+internal const val USE_EXPOSED_JDBC_API_DEPRECATION_MESSAGE = "Use the API in `org.jetbrains.exposed.v1.jdbc` directly."
+internal const val USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE =
+    "Use `buildStatement` in Exposed and the corresponding function API."
 
 // The select queries are not executed eagerly so just use them directly.
 /**
@@ -48,52 +51,95 @@ fun <T : FieldSet> T.selectStatementTableAware(where: TableAwareBuildWhere<T>): 
 /**
  * You can also just use [selectAll].
  */
+@Deprecated(
+    USE_EXPOSED_JDBC_API_DEPRECATION_MESSAGE,
+    ReplaceWith("selectAll()", "org.jetbrains.exposed.v1.jdbc.selectAll")
+)
 fun FieldSet.selectAllStatement() =
     selectAll()
 
 /**
  * You can also just use [select].
  */
+@Deprecated(
+    USE_EXPOSED_JDBC_API_DEPRECATION_MESSAGE,
+    ReplaceWith("select(columns)", "org.jetbrains.exposed.v1.jdbc.select")
+)
 fun ColumnSet.selectStatement(columns: List<Expression<*>>) =
     select(columns)
 
 /**
  * You can also just use [select].
  */
+@Deprecated(
+    USE_EXPOSED_JDBC_API_DEPRECATION_MESSAGE,
+    ReplaceWith("select(column, *columns)", "org.jetbrains.exposed.v1.jdbc.select")
+)
 fun ColumnSet.selectStatement(column: Expression<*>, vararg columns: Expression<*>): Query =
     select(column, *columns)
 
 /**
  * @see org.jetbrains.exposed.v1.jdbc.deleteAll
  */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith("buildStatement { deleteAll() }", "org.jetbrains.exposed.v1.core.statements.buildStatement")
+)
 fun Table.deleteAllStatement() =
-    DeleteStatement(this)
+    buildStatement { deleteAll() }
 
+/**
+ * @see org.jetbrains.exposed.v1.jdbc.deleteWhere
+ * @see StatementBuilder.deleteWhere
+ * @see StatementBuilder.deleteIgnoreWhere
+ * @param offset no longer used
+ */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { if (isIgnore) deleteIgnoreWhere(limit) { where } else deleteWhere(limit) { where } }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun Table.deleteWhereStatement(
     where: WhereOp, isIgnore: Boolean = false, limit: Int? = null, offset: Long? = null
 ): DeleteStatement =
-    // TODO `offset` unused
-    DeleteStatement(targetsSet = this, where, isIgnore, limit, emptyList())
+    buildStatement { if (isIgnore) deleteIgnoreWhere(limit) { where } else deleteWhere(limit) { where } }
 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.deleteWhere].
+ * @param offset no longer used
  */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { deleteWhere(limit, where) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.deleteWhereStatement(
     limit: Int? = null, offset: Long? = null, where: TableAwareWithSqlExpressionBuilderBuildWhere<T>
 ): DeleteStatement =
-    // TODO `offset` unused
-    DeleteStatement(targetsSet = this, where(), false, limit, emptyList())
+    buildStatement { deleteWhere(limit, where) }
 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.deleteWhere].
+ * @param offset no longer used
  */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { deleteIgnoreWhere(limit, where) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.deleteIgnoreWhereStatement(
     limit: Int? = null, offset: Long? = null, where: TableAwareWithSqlExpressionBuilderBuildWhere<T>
 ): DeleteStatement =
-    // TODO `offset` unused
-    DeleteStatement(targetsSet = this, where(), true, limit, emptyList())
+    buildStatement { deleteIgnoreWhere(limit, where) }
 
 // to access the protected `arguments` in the super class
+@Deprecated("No longer needed.", ReplaceWith("InsertStatement<Key>"))
 class HelperInsertStatement<Key : Any>(table: Table, isIgnore: Boolean = false) :
     InsertStatement<Key>(table, isIgnore) {
     override var arguments: List<List<Pair<Column<*>, Any?>>>?
@@ -106,63 +152,102 @@ class HelperInsertStatement<Key : Any>(table: Table, isIgnore: Boolean = false) 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.insert].
  */
-fun <T : Table> T.insertStatement(body: T.(InsertStatement<Number>) -> Unit): HelperInsertStatement<Number> =
-    HelperInsertStatement<Number>(this).apply {
-        body(this)
-    }
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { insert(body) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
+fun <T : Table> T.insertStatement(body: T.(InsertStatement<Number>) -> Unit): InsertStatement<Number> =
+    buildStatement { insert(body) }
 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.insertIgnore].
  */
-fun <T : Table> T.insertIgnoreStatement(body: T.(InsertStatement<Number>) -> Unit): HelperInsertStatement<Number> =
-    HelperInsertStatement<Number>(this, true).apply {
-        body(this)
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { insertIgnore(body) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
+fun <T : Table> T.insertIgnoreStatement(body: T.(InsertStatement<Number>) -> Unit): InsertStatement<Number> =
+    buildStatement {
+        // cast here to avoid changing the signature to match Exposed's API exactly
+        @Suppress("UNCHECKED_CAST")
+        insertIgnore(body as (T.(UpdateBuilder<*>) -> Unit)) as InsertStatement<Number>
     }
 
+@Deprecated("Copied from Exposed. No longer needed.")
 fun Table.defaultColumnsForInsertSelect() =
     columns.filter { !it.columnType.isAutoInc || it.autoIncColumnType?.nextValExpression != null }
 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.insert].
  */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { if (isIgnore) insertIgnore(selectQuery, columns) else insert(selectQuery, columns) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.insertSelectStatement(
     selectQuery: AbstractQuery<*>,
     columns: List<Column<*>> = defaultColumnsForInsertSelect(),
     isIgnore: Boolean = false
 ): InsertSelectStatement =
-    InsertSelectStatement(columns, selectQuery, isIgnore)
+    buildStatement { if (isIgnore) insertIgnore(selectQuery, columns) else insert(selectQuery, columns) }
 
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { update(where?.let { { it } }, limit, body) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.updateStatementWithWhereOp(
     where: WhereOp? = null, limit: Int? = null, body: T.(UpdateStatement) -> Unit
-): UpdateStatement {
-    val query = UpdateStatement(this, limit, where)
-    body(query)
-    return query
-}
+): UpdateStatement =
+    buildStatement { update(where?.let { { it } }, limit, body) }
 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.update].
  */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { update(where, limit, body) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.updateStatement(
     where: BuildWhere? = null, limit: Int? = null, body: T.(UpdateStatement) -> Unit
-): UpdateStatement {
-    val query = UpdateStatement(this, limit, where?.invoke())
-    body(query)
-    return query
-}
+): UpdateStatement =
+    buildStatement { update(where, limit, body) }
 
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { update(where?.let { { it() } }, limit, body) }   ",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.updateStatementTableAware(
-    where: TableAwareBuildWhere<T>? = null, limit: Int? = null, body: T.(UpdateStatement) -> Unit
-): UpdateStatement {
-    val query = UpdateStatement(this, limit, where?.let { it() })
-    body(query)
-    return query
-}
+    where: (T.() -> Op<Boolean>)? = null, limit: Int? = null, body: T.(UpdateStatement) -> Unit
+): UpdateStatement =
+    buildStatement { update(where?.let { { it() } }, limit, body) }
 
 /**
  * Adapted from [org.jetbrains.exposed.v1.jdbc.replace].
  */
+@Deprecated(
+    USE_EXPOSED_API_WITH_BUILD_STATEMENT_DEPRECATION_MESSAGE,
+    ReplaceWith(
+        "buildStatement { replace(body) }",
+        "org.jetbrains.exposed.v1.core.statements.buildStatement"
+    )
+)
 fun <T : Table> T.replaceStatement(body: T.(UpdateBuilder<*>) -> Unit): ReplaceStatement<Long> =
-    ReplaceStatement<Long>(this).apply {
-        body(this)
-    }
+    buildStatement { replace(body) }
--- exposed/src/main/kotlin/com/huanshankeji/exposed/v1/core/Where.kt
+++ exposed/src/main/kotlin/com/huanshankeji/exposed/v1/core/Where.kt
@@ -8,5 +8,10 @@ typealias BuildWhere = () -> Op<Boolean>
 @Deprecated("Renamed to `WhereBuilder`", ReplaceWith("BuildWhere"/*, "com.huanshankeji.exposed.v1.core.BuildWhere"*/))
 typealias Where = BuildWhere
 typealias WhereOp = Op<Boolean>
+// This type tends to make the code harder to understand. Consider just inlining it.
 typealias TableAwareBuildWhere<T/*: FieldSet*/> = T.() -> Op<Boolean>
+@Deprecated(
+    "`SqlExpressionBuilder` is deprecated and removed. This is now identical to `TableAwareBuildWhere`.",
+    ReplaceWith("TableAwareBuildWhere<T>")
+)
 typealias TableAwareWithSqlExpressionBuilderBuildWhere<T> = T.() -> Op<Boolean>
