e1015eba1ad883b9a0341a36385b18a584738454
Yongshun Ye <ShreckYe@gmail.com>
2025-11-19 21:19:50 +0800

    Deprecate `jdbcUrl`

--- core/src/jvmMain/kotlin/com/huanshankeji/jdbc/JdbcUrls.kt
+++ core/src/jvmMain/kotlin/com/huanshankeji/jdbc/JdbcUrls.kt
@@ -1,4 +1,5 @@
 package com.huanshankeji.jdbc
 
+@Deprecated("The JDBC URL format is not actually universal.")
 fun jdbcUrl(rdbms: String, host: String, port: Int?, database: String) =
     "jdbc:$rdbms://$host${port?.let { ":$it" } ?: ""}/$database"
