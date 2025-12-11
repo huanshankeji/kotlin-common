0bc4ba88eb2128523cec2deff6c68cc5f5d597ad
Yongshun Ye <ShreckYe@gmail.com>
2025-12-12 05:39:51 +0800

    Rename `executeBatchAwaitForSqlResultSequence` to `executeBatchCoAwaitForSqlResultSequence`

--- vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/sqlclient/PreparedQuery.kt
+++ vertx/src/main/kotlin/com/huanshankeji/vertx/kotlin/sqlclient/PreparedQuery.kt
@@ -5,5 +5,12 @@ import io.vertx.sqlclient.PreparedQuery
 import io.vertx.sqlclient.SqlResult
 import io.vertx.sqlclient.Tuple
 
-suspend fun <SqlResultT : SqlResult<*>> PreparedQuery<SqlResultT>.executeBatchAwaitForSqlResultSequence(batch: List<Tuple>) =
+suspend fun <SqlResultT : SqlResult<*>> PreparedQuery<SqlResultT>.executeBatchCoAwaitForSqlResultSequence(batch: List<Tuple>) =
     executeBatch(batch).coAwait().batchSqlResultSequence()
+
+@Deprecated(
+    "Use `executeBatchCoAwaitForSqlResultSequence`.",
+    ReplaceWith("this.executeBatchCoAwaitForSqlResultSequence(batch)")
+)
+suspend fun <SqlResultT : SqlResult<*>> PreparedQuery<SqlResultT>.executeBatchAwaitForSqlResultSequence(batch: List<Tuple>) =
+    executeBatchCoAwaitForSqlResultSequence(batch)
