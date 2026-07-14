06ceaad635238ddcc1a44268942b8202319e7f36
Yongshun Ye <shreckye@gmail.com>
2026-07-14 09:45:29 +0800

    Add one more gradle-common repository configuration code link and improve related comments

--- gradle/classpath-bootstrap.gradle.kts
+++ gradle/classpath-bootstrap.gradle.kts
@@ -3,18 +3,22 @@
  * versions and repositories needed before project conventions are on the classpath.
  */
 
-// Currently wires GitHub Packages / mavenLocal for gradle-common *-dev-commit-* artifacts.
+// Currently wires GitHub Packages / mavenLocal for gradle-common *-dev-commit-* artifacts. gradle-common APIs are not on the classpath yet.
 extra["repositories"] = fun RepositoryHandler.() {
     gradlePluginPortal()
+    /*
+    Adapted (simplified) from:
+    https://github.com/huanshankeji/gradle-common/blob/main/kotlin-common/gradle-library/src/main/kotlin/com/huanshankeji/gitversioning/opensourceconvention/Repository.kt
+    */
     exclusiveContent {
         forRepository {
             mavenLocal()
         }
         forRepository {
             /*
-             Copied and adapted from (gradle-common APIs are not on the classpath yet):
-             https://github.com/huanshankeji/gradle-common/blob/main/kotlin-common/gradle-library/src/main/kotlin/com/huanshankeji/github/packages/maven/GithubPackagesMavenRegistry.kt
-             */
+            Adapted from:
+            https://github.com/huanshankeji/gradle-common/blob/main/kotlin-common/gradle-library/src/main/kotlin/com/huanshankeji/github/packages/maven/GithubPackagesMavenRegistry.kt
+            */
             maven {
                 url = uri("https://maven.pkg.github.com/huanshankeji/gradle-common")
                 credentials {
